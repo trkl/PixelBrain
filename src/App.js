14b9f37dca2632be579974f6309b71b514533056
Kristmund Ryggstein <kristmunds@hotmail.com>
2019-02-04 11:36:58 +0000

    Created Resource Manager

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import title from './Images/title.svg';
 import bird from './Images//bird.svg';
 import Game from './Game'
 import './App.css';
+import ResourceManager from './Resource Manager'
 
 class App extends Component {
+  ResourceManager;
+
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
       <div className="App">
@@ -12,8 +18,7 @@ class App extends Component {
           <img src={title} className="App-logo" alt="logo" />        
         </header>
 
-        <img src={bird} className="App-logo" alt="bird" />
-        <Game />
+        <img src={<ResourceManager />} className="App-logo" alt="bird" />
       </div>
     );
   }
