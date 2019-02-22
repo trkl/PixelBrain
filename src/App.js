58d9554c4a452849dccf7829344fdd0cb84de7fb
paturhjortur <6273728+paturhjortur@users.noreply.github.com>
2019-02-22 11:51:53 +0000

    typescript install

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import World from "./World/World";
+import "./App.css";
+import KeyboardObservableProvider from "./InputManager/Context/KeyboardObservableContextProvider";
+import Camera from "./Camera/Camera";
+import { DecInc } from "./GameObject/DecInc";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <i>ReactJS create-react-app + docker boilerplate</i> til Forritaverkfrøði
-          </p>
-          <p>
-            Rætta <code>src/App.js</code> og goym fyri at fáa nýggja útgávu.
-          </p>
-          <a
-            className="App-link"
-            href="https://www.setur.fo/fo/utbugving/bachelor/kt-verkfrodi/skeidslysingar/?educationYearId=493"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Skeiðslýsingar KT-verkfrøði - Náttúruvísindadeildin
-          </a>
-        </header>
-      </div>
+      <KeyboardObservableProvider>
+        <World camera={new Camera()} />
+      </KeyboardObservableProvider>
     );
   }
+  componentDidMount() {}
 }
 
 export default App;
